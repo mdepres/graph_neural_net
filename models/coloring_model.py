3cce243a6204b77202e5471c9072aace8b4395e3
eichhornchen <eichhornchen@crans.org>
2023-05-22 16:07:42 +0200

    Testing another coloring model

--- models/coloring_model.py
+++ models/coloring_model.py
@@ -8,11 +8,12 @@ class ColoringModel(nn.Module):
         self.mlp1 = nn.Linear(embed_dim,hidden_dim)
         self.mlp2 = nn.Linear(hidden_dim, k)
         self.relu = nn.ReLU()
+        self.softmax = nn.Softmax(dim=-1)
 
     
     def forward(self, emb_in):
         out = self.mlp1(emb_in)
         out = self.relu(out)
         out = self.mlp2(out)
-        out = self.relu(out)
+        out = self.softmax(out)
         return out
