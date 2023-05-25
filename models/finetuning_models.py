c11e538f673883eb5468f2d75b407fd78513c610
mdepres <132362623+mdepres@users.noreply.github.com>
2023-05-25 10:40:31 +0200

    Update and rename coloring_model.py to finetuning_models.py

--- models/finetuning_models.py
+++ models/finetuning_models.py
@@ -1,10 +1,11 @@
 import torch
 import torch.nn as nn
 
-class ColoringModel(nn.Module):
-    """ A simple network to learn coloring based on prelearned node embeddings"""
+class Classifier_model(nn.Module):
+    """ A simple network to use prelearned node embeddings to learn graph problems that can be seen as "classification" tasks
+    Exemples : Graph coloring for fixed k or Minimum bisection (k=2) """
     def __init__(self, n_vertices, embed_dim, k, hidden_dim = 64):
-        super(ColoringModel, self).__init__()
+        super(Classifier_model, self).__init__()
         self.mlp1 = nn.Linear(embed_dim,hidden_dim)
         self.mlp2 = nn.Linear(hidden_dim, k)
         self.relu = nn.ReLU()
