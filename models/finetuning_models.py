0313746cf6f489ec7092db91bd77911e170375e8
eichhornchen <eichhornchen@crans.org>
2023-05-25 11:12:53 +0200

    Typo bis

--- models/finetuning_models.py
+++ models/finetuning_models.py
@@ -5,7 +5,7 @@ class ClassifierModel(nn.Module):
     """ A simple network to use prelearned node embeddings to learn graph problems that can be seen as "classification" tasks
     Exemples : Graph coloring for fixed k or Minimum bisection (k=2) """
     def __init__(self, n_vertices, embed_dim, k, hidden_dim = 64):
-        super(Classifier_model, self).__init__()
+        super(ClassifierModel, self).__init__()
         self.mlp1 = nn.Linear(embed_dim,hidden_dim)
         self.mlp2 = nn.Linear(hidden_dim, k)
         self.relu = nn.ReLU()
