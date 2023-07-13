9e66f3e24482cec2f36a51976a03fbdb263cc10b
eichhornchen <eichhornchen@crans.org>
2023-07-13 10:52:08 +0200

    Debugging contiguous?

--- models/trainers.py
+++ models/trainers.py
@@ -340,8 +340,8 @@ class Edge_Classif_Exp(pl.LightningModule):
     def training_step(self, batch, batch_idx):
         logp = self(batch).permute(0,3,1,2) #Putting output dimension before data dimensions
         print("train", logp.shape)
-        loss = self.loss(logp, batch['target'].long())
-        print(loss, logp.contiguous())
+        loss = self.loss(logp.contiguous(), batch['target'].long())
+        print(loss, logp.is_contiguous())
         self.log('train_loss', loss)
         acc = self.accuracy(logp.tensor.rename(None), batch['target'])
         self.log("train_acc", acc)
