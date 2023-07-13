3c018e25bd08d5ca02f2a16cc7aa040d415cd6cd
eichhornchen <eichhornchen@crans.org>
2023-07-13 10:53:21 +0200

    No is_contiguous for masked tensors but contiguous works?

--- models/trainers.py
+++ models/trainers.py
@@ -341,7 +341,6 @@ class Edge_Classif_Exp(pl.LightningModule):
         logp = self(batch).permute(0,3,1,2) #Putting output dimension before data dimensions
         print("train", logp.shape)
         loss = self.loss(logp.contiguous(), batch['target'].long())
-        print(loss, logp.is_contiguous())
         self.log('train_loss', loss)
         acc = self.accuracy(logp.tensor.rename(None), batch['target'])
         self.log("train_acc", acc)
