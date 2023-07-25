70ed679624e5b8352b665c6511ddcf5cddaca811
eichhornchen <eichhornchen@crans.org>
2023-07-25 11:49:45 +0200

    ...

--- commander_explore.py
+++ commander_explore.py
@@ -255,7 +255,7 @@ def predict(config):
 
     gene_test = generator('test', data['test'], path_data_test)
     graph = gene_test.compute_example()
-    print(graph.shape)
+    print(graph)
     graph = collate_classif_predict(graph)
     print(graph['input'].shape)
     
