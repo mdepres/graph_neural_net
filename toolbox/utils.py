998110fe9d61ace058c340713fd5766db87702a2
eichhornchen <eichhornchen@crans.org>
2023-07-25 15:42:30 +0200

    Using strings to describe colors

--- toolbox/utils.py
+++ toolbox/utils.py
@@ -257,7 +257,11 @@ def mbs_pretty_print(adj, edge_classif):
     """ Prints the graph with colors indicating which edges have been classified as internal or external. Uses networkx."""
     adj = adj.cpu().detach().numpy()
     
-    print(edge_classif.shape)
-    edge_color=np.reshape(edge_classif.cpu().detach().numpy(),(-1,))
-    print(edge_color.shape)
+    edge_classif=np.reshape(edge_classif.cpu().detach().numpy(),(-1,))
+    edge_color=[]
+    for el in edge_classif:
+        if el==0:
+            edge_color.append("black")
+        else:
+            edge_color.append("red")
     nx.draw_networkx(nx.from_numpy_array(adj), edge_color=edge_color)
