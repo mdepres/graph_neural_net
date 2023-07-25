5ddce3615be966ce065ecde89a576f9ddf1c034f
eichhornchen <eichhornchen@crans.org>
2023-07-25 11:56:45 +0200

    Wrong input type for collate?

--- commander_explore.py
+++ commander_explore.py
@@ -254,7 +254,7 @@ def predict(config):
         generator = dg.QAP_Generator
 
     gene_test = generator('test', data['test'], path_data_test)
-    graph = gene_test.compute_example()
+    graph = [gene_test.compute_example()]
 #     print(graph)
 #     graph = collate_classif_predict(graph)
 #     print(graph['input'].shape)
