01013f018defd48f773e930ddfeeed602941a139
eichhornchen <eichhornchen@crans.org>
2023-07-28 13:49:50 +0200

    clearer print

--- toolbox/utils.py
+++ toolbox/utils.py
@@ -286,7 +286,9 @@ def mbs_pretty_print(adj, edge_classif, target, groups):
         else:
             node_color.append("red")
     
-    print(edgelist, G.edges())
+    print(edgelist, end="\n\n")
+    
+    print(G.edges())
     #pos = nx.bipartite_layout(G, nodes)
     nx.draw_networkx(G, node_color=node_color, edge_color=edge_color) #pos=pos,
     plt.savefig("graph.png")
